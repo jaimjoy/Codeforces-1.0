d8bf93e99ff60d960b451c2437506e107f75d65c
jaimjoy <jaim81200@gmail.com>
2026-04-17 16:05:24 +0600

    Accepted Solution

---
+++ B_Beautiful_Numbers.cpp
@@ -0,0 +1,53 @@
+//  In the name of Almighty Allah
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+#define nl '\n'
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+#define all(a) (a).begin(),(a).end()
+#define allr(a) (a).rbegin(),(a).rend()
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define minus cout<<"-1\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ll gcd(ll a, ll b) {return b ? gcd(b, a%b):a;}
+ll lcm(ll a, ll b) {return (a*b)/gcd(a,b);}
+#define cin(a) for(auto &x:a) cin>>x;
+string decToBin(ll n){string s="";while(n>0){s=to_string(n%2)+s;n/=2;}return s;}
+
+void solve()
+{
+    string s;cin>>s;
+    vector<int> digits;
+    int sum=0;
+    for(int i=0; i<s.size(); i++)
+    {
+        sum += s[i]-'0';
+        digits.pb((s[i]-'0') - (i==0));
+    }
+
+    sort(all(digits));
+    int ans=0;
+    while(sum >= 10)
+    {
+        sum -= digits.back();
+        digits.pop_back();
+        ans++;
+    }
+    cout<<ans<<nl;
+}
+
+int main()
+{
+    optimize();
+    // solve();
+    ll tc;cin>>tc;for(int i=1; i<=tc; i++)solve();
+    return 0;
+}
+//         Alhamdulillah         //
